7c5935080ec5038dbec62a4aec84a76b718c1e22
Guilherme loli <guilherme.loli@brtk.com.br>
2022-06-13 14:20:55 -0300

    feat: add routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,12 @@
-export {};
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path='/' element={<h1>home</h1>} />
+      <Route path='about' element={<h1>about</h1>} />
+
+      <Route path='*' element={<Navigate to='/' />} />
+    </Routes>
+  );
+}
