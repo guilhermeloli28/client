169d4a92527f971d7543916be7fff82786a6dc18
Guilherme loli <guilherme.loli@brtk.com.br>
2022-06-13 14:39:49 -0300

    feat: add light theme and provider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,17 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<h1>home</h1>} />
+      <Route
+        path='/'
+        element={
+          <Button variant='contained' color='primary'>
+            Teste
+          </Button>
+        }
+      />
       <Route path='about' element={<h1>about</h1>} />
 
       <Route path='*' element={<Navigate to='/' />} />
